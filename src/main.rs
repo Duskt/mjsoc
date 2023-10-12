07235e0abc6dd4f660162e9a167ec4f334d253d9
Duskt <30536860+Duskt@users.noreply.github.com>
2023-10-12 19:14:38 +0100

    wip static files

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use actix_files as fs;
+use actix_web::{get, post, web, App, Error, HttpResponse, HttpServer, Responder};
 use serde::Deserialize;
 
 const IP: &str = "127.0.0.1";
@@ -9,9 +10,11 @@ pub struct UserProfile {
     name: String,
 }
 
+// need to serve all files in public
 #[get("/")]
-async fn hello(info: web::Query<UserProfile>) -> impl Responder {
-    HttpResponse::Ok().body(info.name.clone())
+async fn hello(info: web::Query<UserProfile>) -> Result<fs::NamedFile, Error> {
+    let file = fs::NamedFile::open("public/index.html")?;
+    Ok(file)
 }
 
 #[post("/echo")]
