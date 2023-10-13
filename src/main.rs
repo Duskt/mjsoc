d4167ec217c3f4121617f4c5f00b187d257e77f1
Jacob <hortonjacob1@gmail.com>
2023-10-13 15:19:20 -0400

    feat: use session cookies for auth

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,16 @@
-use std::fmt;
+use uuid::Uuid;
+use actix_web::http::header::LOCATION;
+use std::{
+    collections::HashSet,
+    fmt,
+    sync::{RwLock},
+};
 
 use actix_files as fs;
+use actix_session::{storage::CookieSessionStore, Session, SessionMiddleware};
 use actix_web::{
-    body::BoxBody,
+    body::{ BoxBody },
+    cookie,
     get,
     http::header::{ContentDisposition, DispositionParam, DispositionType},
     web, App, HttpResponse, HttpServer, Responder, ResponseError,
@@ -51,7 +59,7 @@ fn get_qr_url(name: &str) -> Result<String, NameTooLongErr> {
         return Err(NameTooLongErr);
     }
 
-    Ok(format!("{BASE_URL}/login?name={}", name))
+    Ok(format!("{BASE_URL}/register_attendance?name={}", name))
 }
 
 #[get("/")]
@@ -126,17 +134,61 @@ async fn download_qr(info: web::Query<UserProfile>) -> impl Responder {
         .body(binary)
 }
 
-#[get("/login")]
-async fn login(info: web::Query<UserProfile>) -> impl Responder {
+#[get("/register_attendance")]
+async fn register_attendance(info: web::Query<UserProfile>, data: web::Data<AppState>, session: Session) -> impl Responder {
+    if !is_authenticated(&session, &data.authenticated_keys) {
+        return HttpResponse::Found().append_header((LOCATION, format!("{BASE_URL}/login"))).finish()
+    }
+
     HttpResponse::Ok().body(info.name.clone())
+}
+
+// TODO: take redirect url - take them back to the page they were on before
+#[get("/login")]
+async fn login(session: Session, data: web::Data<AppState>) -> impl Responder {
+    if is_authenticated(&session, &data.authenticated_keys) {
+        return HttpResponse::Ok().body("already authenticated");
+    }
+    // TODO: Get and check password
+
+    let uuid = Uuid::new_v4();
+
+    session
+        .insert("session_key", uuid.to_string())
+        .unwrap();
+
+    // TODO: remove oldest if exceeds certain number of keys - maybe best to use Vec?
+    data.authenticated_keys.write().unwrap().insert(uuid.to_string());
+
+    HttpResponse::Ok().body("authenticated now")
+}
+
+fn is_authenticated(session: &Session, authenticated_keys: &RwLock<HashSet<String>>) -> bool {
+    let key = session.get::<String>("session_key").unwrap();
+
+    match key {
+        Some(key) => return authenticated_keys.read().unwrap().contains(&key),
+        None => return false,
+    }
+}
+
+struct AppState {
+    authenticated_keys: RwLock<HashSet<String>>,
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
+        let key = cookie::Key::generate();
+
         App::new()
+            .app_data(web::Data::new(AppState { 
+                authenticated_keys: RwLock::new(HashSet::new())
+            }))
+            .wrap(SessionMiddleware::new(CookieSessionStore::default(), key))
             .service(generate_qr)
             .service(download_qr)
+            .service(register_attendance)
             .service(login)
             .service(fs::Files::new("/", "public").show_files_listing())
     })
