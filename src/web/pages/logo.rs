042db20d9299ec9b0b62fef7dd5f8e8f47ef4c1c
Duskt <30536860+Duskt@users.noreply.github.com>
2024-06-28 15:37:44 +0100

    use logo path in .env

--- src/web/pages/logo.rs
+++ src/web/pages/logo.rs
@@ -2,12 +2,12 @@ use std::env;
 
 use actix_files::NamedFile;
 pub async fn logo() -> Result<NamedFile, std::io::Error> {
-    let logo_file = env::var("LOGO_FILE");
+    let logo_file = env::var("LOGO_PATH");
     println!("Logo requested. Using '{:?}'", logo_file);
     match logo_file {
         Ok(path) => NamedFile::open(path),
         Err(_) => {
-            println!("No env LOGO_FILE found, using default './public/assets/logo.jpg'");
+            println!("No env LOGO_PATH found, using default './public/assets/logo.jpg'");
             NamedFile::open("./public/assets/logo.jpg")
         }
     }
