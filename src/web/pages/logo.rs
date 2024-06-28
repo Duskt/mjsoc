bbbe453fba6feaffdb329fde89efb323a16e8418
Duskt <30536860+Duskt@users.noreply.github.com>
2024-06-28 17:41:01 +0100

    remove print

--- src/web/pages/logo.rs
+++ src/web/pages/logo.rs
@@ -3,7 +3,6 @@ use std::env;
 use actix_files::NamedFile;
 pub async fn logo() -> Result<NamedFile, std::io::Error> {
     let logo_file = env::var("LOGO_PATH");
-    println!("Logo requested. Using '{:?}'", logo_file);
     match logo_file {
         Ok(path) => NamedFile::open(path),
         Err(_) => {
