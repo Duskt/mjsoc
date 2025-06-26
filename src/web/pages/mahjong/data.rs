9a95434c665f516c636dea69ec2cd79bb3e269f4
Duskt <30536860+Duskt@users.noreply.github.com>
2025-06-26 19:07:49 +0100

    dont forget this!

---
+++ src/web/pages/mahjong/data.rs
@@ -0,0 +1,14 @@
+use actix_session::Session;
+use actix_web::{web, HttpResponse, Responder};
+
+use crate::{auth::is_authenticated, AppState};
+
+pub async fn get_data(
+    session: Session,
+    data: web::Data<AppState>,
+) -> impl Responder {
+    if !is_authenticated(&session, &data.authenticated_keys) {
+        return HttpResponse::Unauthorized().finish();
+    }
+    HttpResponse::Ok().json(data.mahjong_data.lock().unwrap().clone())
+}
